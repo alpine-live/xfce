97b7dbca6e7152e492bb9eedf1db380f1eb1827b
paledega <38860643+paledega@users.noreply.github.com>
2022-10-07 07:00:38 +0000

    Update 17g.sh

--- 17g.sh
+++ 17g.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-apk add py3-yaml py3-gobject3 py3-pyparted tzdata util-linux coreutils rsync
+apk add py3-yaml py3-gobject3 py3-pyparted tzdata util-linux coreutils rsync gparted
 apk add make gettext wget
 wget https://gitlab.com/ggggggggggggggggg/17g/-/archive/master/17g-master.zip -O /tmp/17g.zip
 cd /tmp
