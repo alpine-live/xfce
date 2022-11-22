10951d54c85df3f28dceeca5d7b2fcef29676d47
sulincix <parduscix@yandex.ru>
2022-11-22 10:11:51 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git clone https://gitlab.com/tearch-linux/applications-and-tools/teaiso
 cd teaiso
+sudo apt update
 sudo apt install xorriso grub-pc-bin grub-efi mtools make python3 \
     dosfstools e2fsprogs squashfs-tools python3-yaml \
     gcc wget curl unzip xz-utils zstd -y
