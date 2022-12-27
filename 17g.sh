aa4d1d0330a64ddf12c8847c03527bc67a84f619
sulincix <parduscix@yandex.ru>
2022-12-27 18:39:34 +0000

    Update 17g.sh

--- 17g.sh
+++ 17g.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-apk add py3-yaml py3-gobject3 py3-pyparted tzdata util-linux coreutils rsync gparted
+apk add py3-yaml py3-gobject3 py3-pyparted tzdata util-linux coreutils rsync gparted setxkbmap
 apk add make gettext wget
 wget https://gitlab.com/ggggggggggggggggg/17g/-/archive/master/17g-master.zip -O /tmp/17g.zip
 cd /tmp
 unzip 17g.zip
 cd  /tmp/17g-master/
 make && make install
+ln -s true /bin/locale-gen
