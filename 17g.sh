4eaba5ca0c22b50c4ffcb34fe06372b910812e8b
sulincix <parduscix@yandex.ru>
2023-08-03 11:32:27 +0000

    Update 17g.sh

--- 17g.sh
+++ 17g.sh
@@ -2,6 +2,6 @@
 set +e
 cd /tmp
 apk add wget curl
-curl -L https://github.com/alpine-live/17g-alpine/releases/download/current/17g-installer-4.4.1-r0.apk -o 17g.apk
+curl -L https://github.com/alpine-live/17g-alpine/releases/download/4.4.2/17g-installer-4.4.2-r0.apk -o 17g.apk
 apk add *.apk --allow-untrusted
 ln -s true /bin/locale-gen || true
