7c0db216fe3057ee52a0838ba0f7fcd755e87516
sulincix <parduscix@yandex.ru>
2024-03-17 11:51:53 +0000

    Update customize-airootfs.sh

--- customize-airootfs.sh
+++ customize-airootfs.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 apk update
 apk upgrade
-setup-xorg-base || true
+#x11
+apk add xorg-server xf86-input-libinput eudev mesa-dri-gallium
+# xfce
 apk add nano firefox
 apk add xfce4 xfce4-terminal xfce4-screensaver lightdm-gtk-greeter adwaita-icon-theme pavucontrol
-apk add elogind polkit-elogind dbus wpa_supplicant networkmanager network-manager-applet pulseaudio pulseaudio-alsa
-# apps
-apk add flatpak
-# flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# logind and other stuff
+apk add elogind polkit-elogind dbus wpa_supplicant networkmanager \
+    bluez networkmanager-wifi networkmanager-tui librsvg fuse
+# pipewire
+apk add wireplumber pipewire pipewire-pulse pipewire-alsa pipewire-spa-bluez \
+    pipewire-jack pipewire-spa-tools
 echo -e "live\nlive\n" | passwd root
 rc-update add dbus
 rc-update add networkmanager
@@ -16,6 +19,3 @@ rc-update add wpa_supplicant
 rc-update add lightdm
 rc-update add udev
 rc-update add elogind
-# laptop stuff
-apk add acpi cpufreqd pm-utils
-rc-update add cpufreqd
